74700ab203d1aa9c745b8a4d7d661bd3dd0b16e2
reyhaneranjbar <reyhane.ranjbar.93@gmail.com>
2025-12-28 15:34:26 +0330

    mk3

---
+++ src/app/UI/Hero.tsx
@@ -0,0 +1,237 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className=" flex ">
+      <div className="relative flex items-center">
+        <div className="flex flex-row items-center rounded-full bg-gray-700 py-[3.2px] pl-[4.8px] pr-[12.81px] text-white">
+          <img
+            src="/avatar.jpg"
+            alt=""
+            className="w-[25.62px] h-[25.62px] rounded-full"
+          />
+          <p className="font-medium">Your Na..</p>
+        </div>
+
+        <div className="absolute left-full top-50 -translate-x-1/5  ">
+          <svg
+            className="rotate-[20deg]"
+            width="29"
+            height="32"
+            viewBox="0 0 29 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M16.4645 2.15092e-06L28.3184 22.7096L3.25382 14.8277L13.2552 9.6072L13.3779 9.3194L16.4645 2.15092e-06Z"
+              fill="white"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M25.1951 20.1859L16.8973 4.28923L14.8089 10.5611L14.6849 10.8771L7.53097 14.5965L25.1951 20.1859Z"
+              fill="#344054"
+            />
+          </svg>
+          <svg
+            width="76"
+            height="76"
+            viewBox="0 0 76 76"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g filter="url(#filter0_d_6003_347)">
+              <path
+                d="M2.91675 8.51327C2.91675 5.42235 5.42244 2.91666 8.51336 2.91666H67.3201C70.4111 2.91666 72.9167 5.42234 72.9167 8.51326V67.32C72.9167 70.411 70.4111 72.9167 67.3201 72.9167H8.51336C5.42244 72.9167 2.91675 70.411 2.91675 67.32V8.51327Z"
+                fill="white"
+                shape-rendering="crispEdges"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M37.9194 49.7078L38.0126 55.4166C37.1605 55.417 36.3419 55.0856 35.7302 54.4925L35.2844 53.9618L24.4545 41.0834L20.7709 36.6991C20.7612 36.6939 20.7534 36.6858 20.7488 36.6758L20.6906 36.6083L20.6789 36.5932C20.6069 36.4982 20.5492 36.3931 20.5078 36.2813C20.4527 36.1442 20.4224 35.9984 20.4182 35.8506V35.7622C20.4182 35.7343 20.4205 35.7052 20.4264 35.6772C20.4518 35.3905 20.5732 35.1207 20.7709 34.9114C21.0071 34.6553 21.3505 34.4924 21.7276 34.4924H25.3647L37.9183 49.7101L37.9206 49.7078H37.9194Z"
+                fill="#FCD1CA"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M37.9173 42.8619V49.9371L20.7454 29.4459C20.6423 29.3304 20.5611 29.1971 20.5057 29.0525C20.4513 28.914 20.4214 28.7671 20.4172 28.6184V28.5311C20.4265 28.2029 20.5581 27.9084 20.7699 27.6838C21.005 27.4242 21.3483 27.2648 21.7266 27.2648H24.9831L37.9173 42.8619Z"
+                fill="#FF9D8D"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M21.7194 20.4177C21.3447 20.4177 21.0002 20.576 20.7674 20.8343C20.5579 21.0543 20.4275 21.3523 20.4182 21.677V21.7631C20.4217 21.9179 20.452 22.0622 20.5067 22.1937C20.5078 22.2065 20.5137 22.217 20.5183 22.2287C20.5751 22.3655 20.6564 22.4909 20.7581 22.5988L20.7767 22.6197L37.9171 42.8619V55.4166C38.7643 55.4184 39.5786 55.0894 40.1867 54.4995C40.1902 54.4995 40.1902 54.4995 40.1902 54.4972L55.0878 36.8109C55.1064 36.7923 55.1227 36.7713 55.139 36.7504C55.2182 36.6526 55.281 36.542 55.3276 36.421C55.3858 36.2767 55.416 36.1184 55.416 35.9496V21.7177C55.4175 21.3878 55.2916 21.0701 55.0645 20.8308C54.9433 20.6992 54.7958 20.5943 54.6317 20.523C54.4675 20.4516 54.2903 20.4154 54.1113 20.4165H21.7206L21.7194 20.4177Z"
+                fill="#FF6661"
+              />
+            </g>
+            <defs>
+              <filter
+                id="filter0_d_6003_347"
+                x="8.13007e-05"
+                y="-1.0252e-05"
+                width="75.8333"
+                height="75.8333"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+              >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feColorMatrix
+                  in="SourceAlpha"
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                  result="hardAlpha"
+                />
+                <feOffset />
+                <feGaussianBlur stdDeviation="1.45833" />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.3 0"
+                />
+                <feBlend
+                  mode="normal"
+                  in2="BackgroundImageFix"
+                  result="effect1_dropShadow_6003_347"
+                />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="effect1_dropShadow_6003_347"
+                  result="shape"
+                />
+              </filter>
+            </defs>
+          </svg>
+        </div>
+      </div>
+
+      <div className="flex items-center">
+        <img src="/portfolio.jpg" alt="" className="mx-[455.15px]" />
+      </div>
+      <div className="relative inline-flex w-fit h-fit">
+        <svg
+          className="absolute -top-8 -left-8 z-0"
+          width="76"
+          height="76"
+          viewBox="0 0 76 76"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g filter="url(#filter0_d_6003_601)">
+            <rect
+              x="2.91663"
+              y="2.91669"
+              width="70"
+              height="70"
+              rx="5.59661"
+              fill="white"
+              shapeRendering="crispEdges"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M37.9166 37.9167C37.9166 34.6321 40.5793 31.9694 43.8639 31.9694C47.1485 31.9694 49.8112 34.6321 49.8112 37.9167C49.8112 41.2013 47.1485 43.864 43.8639 43.864C40.5793 43.864 37.9166 41.2013 37.9166 37.9167Z"
+              fill="#1ABCFE"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M26.0221 49.8112C26.0221 46.5266 28.6848 43.864 31.9694 43.864H37.9166V49.8112C37.9166 53.0958 35.2539 55.7585 31.9694 55.7585C28.6848 55.7585 26.0221 53.0958 26.0221 49.8112Z"
+              fill="#0ACF83"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M37.9166 20.0749V31.9694H43.8639C47.1485 31.9694 49.8112 29.3067 49.8112 26.0222C49.8112 22.7376 47.1485 20.0749 43.8639 20.0749H37.9166Z"
+              fill="#FF7262"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M26.0221 26.0222C26.0221 29.3067 28.6848 31.9694 31.9694 31.9694H37.9166V20.0749H31.9694C28.6848 20.0749 26.0221 22.7376 26.0221 26.0222Z"
+              fill="#F24E1E"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M26.0221 37.9167C26.0221 41.2013 28.6848 43.864 31.9694 43.864H37.9166V31.9694H31.9694C28.6848 31.9694 26.0221 34.6321 26.0221 37.9167Z"
+              fill="#A259FF"
+            />
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_6003_601"
+              x="-4.07696e-05"
+              y="2.02656e-05"
+              width="75.8333"
+              height="75.8333"
+              filterUnits="userSpaceOnUse"
+              colorInterpolationFilters="sRGB"
+            >
+              <feFlood floodOpacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset />
+              <feGaussianBlur stdDeviation="1.45833" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.3 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_6003_601"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_6003_601"
+                result="shape"
+              />
+            </filter>
+          </defs>
+        </svg>
+        <svg
+          className="absolute -top-3 -left-3 z-10"
+          width="29"
+          height="32"
+          viewBox="0 0 29 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M11.9748 31.6228L0 8.68172L25.3201 16.644L15.2167 21.9177L15.0928 22.2084L11.9748 31.6228Z"
+            fill="white"
+          />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M3.15515 11.2311L11.5375 27.2899L13.6472 20.954L13.7725 20.6348L20.9994 16.8775L3.15515 11.2311Z"
+            fill="#344054"
+          />
+        </svg>
+       
+      </div>
+       <button className="flex leading-none relative z-20 flex-row items-center rounded-full bg-gray-700 text-white py-[3.23px] pl-[4.85px] pr-[12.94px]  whitespace-nowrap">
+          <img
+            src="/avatar.jpg"
+            alt=""
+            className="w-[25.88px] h-[25.88px] rounded-full"
+          />
+          <span className="font-medium">Your Na..</span>
+        </button>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Hero from "./UI/Hero";
+
+export default function Home() {
+  return (
+    <>
+    
+    <Hero/>
+    
+    
+    </>
+  );
+}
